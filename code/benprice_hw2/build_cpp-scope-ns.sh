460fdec756f8374d5cf102e122f488a94ab0a3fd
Benjamin Price <benprice@uw.edu>
2025-05-05 20:46:42 -0700

    Add matrix class and utility functions for matrix operations
    - Implemented a Matrix class template with constructors, accessors, and operations such as addition, multiplication, and transposition.
    - Added utility functions for reading and writing binary matrices, as well as generating column-major matrices.
    - Created demonstration programs for matrix operations and IO bandwidth measurement.
    - Added scripts for cleaning and compiling the project.
    - Included CSV output for benchmarking matrix IO performance and plotting capabilities for visualizing results.
    - Implemented file swap functions for row and column operations on matrices stored in binary files.

--- code/benprice_hw2/build_cpp-scope-ns.sh
+++
@@ -1,7 +0,0 @@
-clear
-ls -lstr code/learning
-g++ -std=c++17 -c code/learning/cpp-scope-ns.cpp -o code/learning/cpp-scope-ns.o
-ls -lstr code/learning
-g++ -o code/learning/xcpp-scope-ns code/learning/cpp-scope-ns.o
-ls -lstr code/learning
-./code/learning/xcpp-scope-ns
--- code/benprice_hw2/build_cpp-types.sh
+++
@@ -1,7 +0,0 @@
-clear
-ls -lstr code/learning
-g++ -std=c++17 -c code/learning/cpp-types.cpp -o code/learning/cpp-types.o
-ls -lstr code/learning
-g++ -o code/learning/xcpp-types code/learning/cpp-types.o
-ls -lstr code/learning
-./code/learning/xcpp-types
